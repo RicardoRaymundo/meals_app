0bdfbffc92f7d7318d90095b934812fd125a6a58
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-06 15:15:28 +0000

    FEAT: Added gradient color to category item box

--- lib/components/category_item.dart
+++ lib/components/category_item.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:flutter/material.dart';
 import 'package:meals_app/models/category.dart';
 
@@ -10,6 +8,17 @@ class CategoryItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(category.title);
+    return Container(
+      padding: const EdgeInsets.all(15),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(15),
+        gradient: LinearGradient(
+          colors: [category.color.withOpacity(0.5), category.color],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: Text(category.title, style: Theme.of(context).textTheme.titleMedium,),
+    );
   }
 }
