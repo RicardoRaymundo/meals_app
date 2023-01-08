c1925e100df8b2beb58c817547d138ac993f2044
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-08 15:49:43 +0000

    FEAT: updated categories screen

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -7,21 +7,16 @@ class CategoriesScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Vamos cozinhar?'),
+    return GridView(
+      padding: const EdgeInsets.all(5),
+      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+        maxCrossAxisExtent: 200,
+        childAspectRatio: 3 / 2,
+        crossAxisSpacing: 20,
+        mainAxisSpacing: 20,
       ),
-      body: GridView(
-        padding: const EdgeInsets.all(5),
-        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-          maxCrossAxisExtent: 200,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 20,
-          mainAxisSpacing: 20,
-        ),
-        children:
-            dummyCategories.map((category) => CategoryItem(category)).toList(),
-      ),
+      children:
+          dummyCategories.map((category) => CategoryItem(category)).toList(),
     );
   }
 }
