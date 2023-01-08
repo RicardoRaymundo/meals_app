0730f0e8cc6ea6c75870c6e9b55c63ec75309026
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-08 15:49:53 +0000

    FEAT: updated main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/meal.dart';
+import 'package:meals_app/models/settings.dart';
 import 'package:meals_app/screens/categories_meals_screen.dart';
 import 'package:meals_app/screens/categories_screen.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
+import 'package:meals_app/screens/settings_screen.dart';
+import 'package:meals_app/screens/tabs_screen.dart';
 import 'package:meals_app/utils/app_routes.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  Settings settings = Settings();
+  List<Meal> _availableMeals = dummyMeals;
+  List<Meal> _favoriteMeals = [];
+
+  void _filterMeals(Settings settings) {
+    setState(() {
+      this.settings = settings;
+      _availableMeals = dummyMeals.where((meal) {
+        final filterGluten = settings.isGlutenFree && !meal.isGlutenFree;
+        final filterLactose = settings.isLactoseFree && !meal.isLactoseFree;
+        final filterVegan = settings.isVegan && !meal.isVegan;
+        final filterVegetarian = settings.isVegetarian && !meal.isVegetarian;
+
+        return !filterGluten &&
+            !filterLactose &&
+            !filterVegan &&
+            !filterVegetarian;
+      }).toList();
+    });
+  }
+
+  void _toggleFavorite(Meal meal) {
+    setState(() {
+      _favoriteMeals.contains(meal)
+          ? _favoriteMeals.remove(meal)
+          : _favoriteMeals.add(meal);
+    });
+  }
+
+  bool _isFavorite(Meal meal) {
+    return _favoriteMeals.contains(meal);
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +67,7 @@ class MyApp extends StatelessWidget {
         ),
         primarySwatch: Colors.pink,
         fontFamily: 'Raleway',
-        canvasColor: Color.fromRGBO(255, 254, 229, 1),
+        canvasColor: const Color.fromRGBO(255, 254, 229, 1),
         textTheme: ThemeData.light().textTheme.copyWith(
               titleMedium: const TextStyle(
                 fontFamily: 'RobotoCondensed',
@@ -34,10 +77,31 @@ class MyApp extends StatelessWidget {
       ),
       // home: const CategoriesScreen(),
       routes: {
-        AppRoutes.home: (context) => const CategoriesScreen(),
-        AppRoutes.categoriesMeals: (context) => const CategoriesMealsScreen(),
-        AppRoutes.mealDetail: (context) => const MealDetailScreen()
+        AppRoutes.home: (context) => TabsScreen(_favoriteMeals),
+        AppRoutes.categoriesMeals: (context) =>
+            CategoriesMealsScreen(_availableMeals),
+        AppRoutes.mealDetail: (context) => MealDetailScreen(_toggleFavorite, _isFavorite),
+        AppRoutes.settings: (context) => SettingsScreen(settings, _filterMeals)
       },
+
+      // METODOS DE EVENTO DE NAVEGAÇÂO
+      // UTIL PARA ATIVAR FUNCS PARA ALGUMA NAVEGAÇÃO EM ESPECIFICO
+      // onGenerateRoute: (settings) {
+      //   if (settings.name == '/alguma-coisa') {
+      //     return null;
+      //   } else if (settings.name == '/outra-coisa') {
+      //     return null;
+      //   } else {
+      //     return MaterialPageRoute(builder: (_) {
+      //       return CategoriesScreen();
+      //     });
+      //   }
+      // },
+      // onUnknownRoute: (settings) {
+      //   return MaterialPageRoute(builder: (_) {
+      //     return CategoriesScreen();
+      //   });
+      // },
     );
   }
 }
