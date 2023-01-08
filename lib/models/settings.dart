bd083b948dc27c78a2fb84670d293c26f940f149
RicardoRaymundo <ricardoraymundao@gmail.com>
2023-01-08 15:47:57 +0000

    FEAT: created settings model

---
+++ lib/models/settings.dart
@@ -0,0 +1,13 @@
+class Settings {
+  Settings({
+    this.isGlutenFree = false,
+    this.isLactoseFree = false,
+    this.isVegan = false,
+    this.isVegetarian = false,
+  });
+
+  bool isGlutenFree;
+  bool isLactoseFree;
+  bool isVegan;
+  bool isVegetarian;
+}
